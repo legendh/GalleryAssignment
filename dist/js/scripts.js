74202bcdc747a77d34647b71f980fdd75054728c
legendh <h_ergoksen@hotmail.com>
2017-01-26 14:12:11 +0300

    Fixed somebug about frontend

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -12598,27 +12598,20 @@ if (typeof jQuery === 'undefined') {
 }(jQuery);
 
 $(document).ready(function(){
-    var filelength = 3;
     $('.cstm-btn-add').click(function () {
-        $('.container .row').append('<div class="col-md-4"><img class="thumbnail thumbnail-img" src="http://i.imgur.com/YpHAm.jpg"/></div>');
+        $('.img-gallery').append('<div class="col-md-4"><img class="thumbnail thumbnail-img" src="http://i.imgur.com/YpHAm.jpg"/></div>');
     });
     
     $('.cstm-show-img').click(function(){
         var myImgUrl = $('.url-field').val();
-        $('.container .row').append('<div class="col-md-4"><img class="thumbnail thumbnail-img" src="'+myImgUrl+'"/></div>');
+        $('.img-gallery').append('<div class="col-md-4"><img class="thumbnail thumbnail-img" src="'+myImgUrl+'"/></div>');
     });
 
-    $('.thumbnail-img').on('click',function(){
-        console.log('img');
-        $(this).val();
-    });
-    
+
     $('.cstm-btn-remove').click(function(){
-        console.log('Test 2');
+        console.log('Test 3');
         var numberOfImg = $('.container .row .col-md-4').length;
             $('.container .row .col-md-4:nth-child('+numberOfImg+')').remove();
-
-
 
     });
     
